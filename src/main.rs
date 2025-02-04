9cf8eb50b4b4fbebe912b471d75ad4788c1a35cf
Vladimir Ilyin <ilyinvp@yandex.ru>
2025-02-04 19:25:29 +0300

    deployment

--- src/main.rs
+++ src/main.rs
@@ -100,7 +100,7 @@ fn create_file_handler(key: &String, value: &String) -> impl Fn(&std::path::Path
 }
 
 fn print_usage() {
-    println!("Usage: localizer [OPTIONS]");
+    println!("Usage: app [OPTIONS]");
     println!("Options:");
     println!("  --path    Relative path to locales folder");
     println!("  --key     JSON key to update (e.g., 'common.greeting')");
